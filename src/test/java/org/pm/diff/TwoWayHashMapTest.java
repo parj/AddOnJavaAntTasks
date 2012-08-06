2e2d5f965da8d305fef02c1b7b2e4d87fd462ead
Parjanya Mudunuri <parjanya@gmail.com>
2012-08-06 22:42:08 +0530

    Updated pom to use UTF-8 and added TwoWayHashMap JUNIT test

---
+++ src/test/java/org/pm/diff/TwoWayHashMapTest.java
@@ -0,0 +1,46 @@
+package org.pm.diff;
+
+import junit.framework.TestCase;
+
+public class TwoWayHashMapTest extends TestCase {
+	private TwoWayHashMap twoMap;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		twoMap = new TwoWayHashMap();
+		twoMap.put("luhIUHi", 99000);
+		twoMap.put(298, "KLUHa");
+		twoMap.put("l;kJ", 9812);
+		twoMap.put(28, "`;lij");
+		twoMap.put("^&%VETI", 1298);
+		twoMap.put(287149, "I&To7y8");
+		twoMap.put("lhnILBI8�Č��", 98912387);
+		twoMap.put(28723, "�^wh");
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		twoMap = null;
+	}
+
+	public void testPutStringInteger() {
+		twoMap.put("ABCD", 1234);
+		assertEquals(twoMap.get(1234), "ABCD");
+	}
+
+	public void testPutIntegerString() {
+		twoMap.put(5678, "EFGH");
+		assertEquals((Integer)twoMap.get("EFGH"), new Integer(5678));
+	}
+
+	public void testRemoveString() {
+		twoMap.remove(1234);
+		assertFalse(twoMap.containsKey("ABCD"));
+	}
+
+	public void testRemoveInteger() {
+		twoMap.remove("EFGH");
+		assertFalse(twoMap.containsKey(5678));
+	}
+
+}
