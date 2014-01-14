0abd8228361a8be44963219a849e7e570cfc7169
Parjanya Mudunuri <parjanya@gmail.com>
2014-01-14 13:52:02 +0530

    Comments added

--- src/main/java/uk/co/firstzero/xml/ModifyPath.java
+++ src/main/java/uk/co/firstzero/xml/ModifyPath.java
@@ -23,43 +23,83 @@ package uk.co.firstzero.xml;
 
 import org.apache.tools.ant.Task;
 
+/**
+ * Used for holding the xpath to be modified and the value it needs to be modified or deleted
+ */
 public class ModifyPath extends Task {
 	private String path;
 	private String value;
 	private boolean delete;
-	
+
+    /**
+     * Empty constructor
+     */
 	public ModifyPath() {}
-	
+
+    /**
+     * Constructor of ModifyPath to modify a node based on xPATH
+     * @param path xPATH to be modified
+     * @param value The new value that must be set
+     */
 	public ModifyPath(String path, String value) {
 		this.path = path;
 		this.value = value;
 	}
-	
+
+    /**
+     * Constructor of ModifyPath to delete a node based on xPATH
+     * @param path xPATH to be deleted
+     * @param delete Set true to delete node
+     */
 	public ModifyPath(String path, boolean delete) {
 		this.path = path;
 		this.delete = delete;
 	}
 
+    /**
+     * Sets the xPATH
+     * @param path xPATH
+     */
 	public void setPath(String path) {
 		this.path = path;
 	}
 
+    /**
+     * Gets the xPATH
+     * @return xPATH
+     */
 	public String getPath() {
 		return path;
 	}
 
+    /**
+     * Sets the new value
+     * @param value Sets the new value
+     */
 	public void setValue(String value) {
 		this.value = value;
 	}
 
+    /**
+     * Gets the new value
+     * @return Gets the new value
+     */
 	public String getValue() {
 		return value;
 	}
 
+    /**
+     * Sets the boolean value of whether the node is to be deleted
+     * @param delete Sets the boolean value of whether the node is to be deleted
+     */
 	public void setDelete(boolean delete) {
 		this.delete = delete;
 	}
 
+    /**
+     * Returns the value of delete
+     * @return Returns the value of delete
+     */
 	public boolean isDelete() {
 		return delete;
 	}
